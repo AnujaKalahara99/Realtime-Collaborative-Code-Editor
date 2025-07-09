797120137654056530accc6b3d813203a2199736
AnujaKalahara99 <a.p.kalhara@gmail.com>
2025-07-09 11:02:29 +0530

    changed all to es module

--- producer/producer.js
+++ producer/producer.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { Queue } = require('bullmq');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Queue } from 'bullmq';
 import IORedis from 'ioredis';
 
 const connection = new IORedis({ maxRetriesPerRequest: null });
