64cc8df40e6f1a095f829bbcaf6bacc645cde5ff
AnujaKalahara99 <a.p.kalhara@gmail.com>
2025-07-09 08:04:30 +0530

    removed redis worker index js

--- worker-js/index.js
+++ worker-js/index.js
@@ -1,22 +1,21 @@
 const { Worker } = require('bullmq');
-const Redis = require('ioredis');
-
-const redis = new Redis({
-  host: 'redis',
-  port: 6379,
-  maxRetriesPerRequest: null, //REQUIRED for BullMQ
-});
 
 new Worker('code-execution', async job => {
   if (job.data.language !== 'javascript') return;
 
   return new Promise((resolve) => {
-  try {
+    try {
       const fn = new Function('input', job.data.code);
       const output = fn(job.data.input);
-      resolve({success: true, output: output.toString()});
+      resolve({ success: true, output: output.toString() });
     } catch (err) {
       resolve({ success: false, error: err.message });
     }
   });
-}, { connection : redis });
+}, {
+  connection: {
+    host: 'redis',
+    port: 6379,
+    maxRetriesPerRequest: null // ✅ MUST be null
+  }
+});
