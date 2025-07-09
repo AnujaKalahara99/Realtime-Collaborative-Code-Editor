f421327b36382e2d63abf7f736e10c36aa72913c
AnujaKalahara99 <a.p.kalhara@gmail.com>
2025-07-09 11:00:20 +0530

    changed local host to redi 2

--- worker-js/index.js
+++ worker-js/index.js
@@ -1,12 +1,12 @@
 import { Worker, RedisConnection } from 'bullmq';
 import IORedis from 'ioredis';
 
-const redis = new IORedis('redis://redis:6379', {
+const connection = new IORedis('redis://redis:6379', {
   maxRetriesPerRequest: null,
 });
 
 // Wrap in BullMQ's RedisConnection (REQUIRED)
-const connection = new RedisConnection(redis);
+// const connection = new RedisConnection(redis);
 
 new Worker('WorkerJs', async job => {
   if (job.data.language !== 'javascript') return;
