086bd7ddc04d2c778e377cbf1421227674fb53e6
AnujaKalahara99 <a.p.kalhara@gmail.com>
2025-08-06 19:36:58 +0530

    commited befor merge

--- Frontend/src/Dashboard/CreateCodespaceCard.tsx
+++ Frontend/src/Dashboard/CreateCodespaceCard.tsx
@@ -13,13 +13,10 @@ function CreateCodespaceCard({ viewMode, onClick }: Props) {
   return (
     <div
       onClick={onClick}
-      className={`${theme.surface} ${
-        theme.border
-      } border-2 border-dashed rounded-lg p-6 flex flex-col items-center justify-center cursor-pointer ${
-        theme.hover
-      } transition-colors min-h-[200px] ${
-        viewMode === "list" ? "!min-h-[80px] flex-row justify-start" : ""
-      }`}
+      className={`${theme.surface} ${theme.border} border-2 border-dashed rounded-lg p-6 flex flex-col items-center 
+      justify-center cursor-pointer ${theme.hover} transition-colors min-h-[200px] 
+      ${viewMode === "list" ? "!min-h-[80px] flex-row justify-start" : ""
+        }`}
     >
       <Plus
         size={viewMode === "grid" ? 48 : 24}
